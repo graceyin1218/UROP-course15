cbd683ef0075bea51d67ba9b43a7a16c5b53c60e
graceyin1218 <graceyin1218@gmail.com>
2016-05-02 01:46:48 -0400

    android csv maker, old

--- android_csv_maker.js
+++ android_csv_maker.js
@@ -12,9 +12,10 @@ var calculateDate = function(rawdate) {
   return timestamp;
 };
 
-var file = "android_gaming.csv";
+var file = "android_gaming_all.csv";
 var fields = ["app_id", "reportdate", "appname", "content_rating", "overallrating", "cur_version", "num_ratings", "permission1", "permission2", "permission3", "permission4", "permission5", "permission6", "permission7", "permission8", "permission9", "permission10", "size", "app_url", "android_requirement", "devname", "dev_website", "category", "price", "num_reviews", "installs", "appdescription", "phone_only"];
 var results = {};
+var appIDs = [];
 
 // key = app id.
 // value = (called "value") the whole doc... (including the id)
@@ -47,6 +48,7 @@ var filterNewlines = function(s) {
   return replaceAll(s, ",", "");
 };
 
+var headers_written = false;
 
 // TODO: eventually find a way to erase android_gaming.csv before starting to write to it...
 // but for now, deleting/renaming it before running this program should be fine O:)
@@ -54,12 +56,15 @@ var filterNewlines = function(s) {
 var write = function() {
   // HEADERS
 
-  fs.appendFileSync(file, "app_id");
-  for (var i = 1; i < fields.length; i++){
-    fs.appendFileSync(file, "," + fields[i]);
+  if (!headers_written)
+  {
+    fs.appendFileSync(file, "app_id");
+    for (var i = 1; i < fields.length; i++){
+      fs.appendFileSync(file, "," + fields[i]);
+    }
+    fs.appendFileSync(file, "\n");
+    headers_written = true;
   }
-  fs.appendFileSync(file, "\n");
-
 
   // ROWS
 
@@ -87,7 +92,8 @@ MongoClient.connect("mongodb://" + stanforddb.url + "/google_play", function(err
   console.log("connected to database");
   android = db.collection("main_sql");
 
-  var cursor = android.find({"category":"Entertainment"});
+  //var cursor = android.find({"category":"Entertainment"});
+  var cursor = android.find({});
 
   count = 0;
 
@@ -95,6 +101,12 @@ MongoClient.connect("mongodb://" + stanforddb.url + "/google_play", function(err
     count++;
     if (count % 1000 == 0) {
       console.log(count);
+    }
+
+    //need to free memory every once in a while..
+    if (count % 100000 == 0) {
+      write();
+      results = {};
     }
 
     if (err) {
@@ -105,26 +117,34 @@ MongoClient.connect("mongodb://" + stanforddb.url + "/google_play", function(err
     if (doc == null) {
       console.log("\n\ndone\n\n");
       write();
-      console.log("written to csv file: android_gaming.csv");
+      console.log("written to csv file: " + file);
       db.close();
       return;
     }
 
-    var newID = doc["app_id"];
-    var newTimestamp = calculateDate(doc["reportdate"]);
 
-    if (newID in results) {
-      if (newTimestamp < results[newID]["timestamp"]) {
-        results[newID]["timestamp"] = newTimestamp;
-        results[newID]["value"] = doc;
-      }
+    var newID = doc["app_id"];
+//    var newTimestamp = calculateDate(doc["reportdate"]);
+
+
+// for android_gaming_all.csv , just take one instance of each app. Doesn't necessarily need to be the earliest.
+
+    if (newID in appIDs) {
+//      if (newTimestamp < results[newID]["timestamp"]) {
+//        results[newID]["timestamp"] = newTimestamp;
+//        results[newID]["value"] = doc;
+//      }
     }
     else {
+      appIDs.push(newID);
       results[newID] = {};
-      results[newID]["timestamp"] = newTimestamp;
+//      results[newID]["timestamp"] = newTimestamp;
       results[newID]["value"] = doc;
     }
 
+//    //print EVERYTHING!!
+//    results[count] = {};
+//    results[count]["value"] = doc;
   });
 
 });
