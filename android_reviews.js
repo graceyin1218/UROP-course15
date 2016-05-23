36a01314102beb53fcd12628cc812f7cf0fd7b83
graceyin1218 <graceyin1218@gmail.com>
2016-05-23 02:56:32 -0400

    half second pause review scraper

--- android_reviews.js
+++ android_reviews.js
@@ -2,6 +2,7 @@ var unirest = require("unirest");
 var MongoClient = require("mongodb").MongoClient;
 var stanforddb = require("./stanford-db.js");
 var request = require("sync-request");
+var sleep = require("sleep");
 
 
 // These code snippets use an open-source library. http://unirest.io/nodejs
@@ -29,14 +30,22 @@ MongoClient.connect("mongodb://" + stanforddb.url + "/google_play", function(err
 	}
 	console.log("connected to database");
 	global_db = db;
-  var android = db.collection("main_sql");
+  	var android = db.collection("main_sql");
 	var reviews = db.collection("reviews");
 
-	var cursor = android.find({}, {"app_id":1, "appname":1, "_id":0}).limit(25000); //.limit(1);
 	count = 0;
 
-	cursor.each(function(err, doc) {
-		count++;
+
+//synchronous way of iterating over a mongodb cursor
+// http://stackoverflow.com/questions/18119387/iterating-over-a-mongodb-cursor-serially-waiting-for-callbacks-before-moving-to
+
+android.find({}, function(err, resultCursor) {
+	// I believe item == mongodb document
+  function processItem(err, doc) {
+    	if(doc === null) {
+      		return; // All done!
+    	}
+    	count++;
 		if (count%10000 == 0) {
 			console.log(count);
 		}
@@ -80,21 +89,90 @@ MongoClient.connect("mongodb://" + stanforddb.url + "/google_play", function(err
   				"dateIso": r["dateIso"]
   			};
 //  		console.log(newDoc);
+			// this is asynchronous
             reviews.insert(newDoc, function(err, records) {
                 if (err) throw err;
                 // do nothing
             });
         }
 
-        if (callback_count == 3296514) {
-           process.exit();
-        }
+        // wait 0.5 seconds for the review scraping to finish for each document
+        sleep.sleep(0.5);
 
-		});
+    	externalAsyncFunction(doc, function(err) {
+      		resultCursor.nextObject(processItem);
+    	});
 
-          if (callback_count == 25000) {    //3296514) {
-              process.exit();
-          }
+  }
+
+  resultCursor.nextObject(processItem);
+} 
+
+// Mongodb's cursor.each is asynchronous. We want a synchronous version. (look above)
+
+// 	var cursor = android.find({}, {"app_id":1, "appname":1, "_id":0}).limit(25000); //.limit(1);
+// 	count = 0;
+
+// 	cursor.each(function(err, doc) {
+// 		count++;
+// 		if (count%10000 == 0) {
+// 			console.log(count);
+// 		}
+// 		if (err) {
+// 			console.log("ERROR");
+// 			// Cannot close prematurely, because of stupid callbacks
+//       //db.close();
+// 			throw err;
+// 		}
+// 		if (doc == null) {
+// 			console.log("\n\ndone\n\n");
+// 			// Cannot close prematurely. Look above.
+//       //db.close();
+// 			return;
+// 		}
+
+// 		var app_id = doc["app_id"];
+// 		var app_name = doc["appname"];
+
+// 		// Synchronous HTTP request!!
+
+// 		var res = request("GET", "https://gplaystore.p.mashape.com/applicationReviews?id=" + app_id + "&lang=en&page=2", {
+// 			headers: {
+// 				"X-Mashape-Key": "lCbaNKES1SmshAXGZwfFKIClgm7Tp1akcNVjsnvEiCJ9kN0S3H",
+// 				"Accept": "application/json"
+// 			}
+// 		});
+
+// 		//console.log(JSON.parse(res.body));
+
+
+//   		for (var i = 0; i < res.body.length; i++) {
+//   			var r = res.body[i];
+// 			var newDoc = {
+//   				"app_id": app_id,
+//   				"app_name": app_name,
+//   				"title": r["title"],
+//   				"body": r["body"],
+//   				"rating": r["rating"],
+//   				"date": r["date"],
+//   				"dateIso": r["dateIso"]
+//   			};
+// //  		console.log(newDoc);
+//             reviews.insert(newDoc, function(err, records) {
+//                 if (err) throw err;
+//                 // do nothing
+//             });
+//         }
+
+//         if (callback_count == 3296514) {
+//            process.exit();
+//         }
+
+// 		});
+
+//           if (callback_count == 25000) {    //3296514) {
+//               process.exit();
+//           }
 
 
 // original asynchronous version. A lot has been copied and pasted from this.
